75ae9c051eb9f199158e7997a92120d36b491976
Andrei <l2.ls@yandex.ru>
2022-09-17 21:08:20 +0800

    App architecture

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import 'reflect-metadata';
+import { Container } from 'inversify';
+import Application from './cli-application/application.js';
+import LoggerService from './common/logger/logger.service.js';
+import { LoggerInterface } from './common/logger/logger.interface.js';
+import ConfigService from './common/config/config.service.js';
+import { ConfigInterface } from './common/config/config.interface.js';
+import { Component } from './types/component.type.js';
+
+const applicationContainer = new Container();
+
+applicationContainer.bind<Application>(Component.Application).to(Application).inSingletonScope();
+applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService).inSingletonScope();
+applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService).inSingletonScope();
+
+const application = applicationContainer.get<Application>(Component.Application);
+
+application.init();
